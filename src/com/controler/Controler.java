a817cb6387bf8313dae3437812401b317894530c
name <cybersteer@naver.com>
2022-06-20 11:34:10 +0900

    delete file

--- src/com/controler/Controler.java
+++ src/com/controler/Controler.java
@@ -10,24 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.command.GameCommand;
 import com.command.GameContentCommand;
 import com.command.GameContentSelectCommand;
-import com.command.GameImgCommand;
-import com.command.GameImgSelectCommand;
 import com.command.GameItemCommand;
 import com.command.GameItemSelectAllCommand;
-import com.command.GameSelectAllCommand;
-import com.command.GameSelectCommand;
-import com.command.MinimumCommand;
-import com.command.MinimumSelectCommand;
-import com.command.RecommendCommand;
-import com.command.RecommendSelectCommand;
 import com.command.UserCommand;
 import com.command.UserInsertCommand;
 import com.command.UserSelectCommand;
-import com.dto.GameContentDto;
-import com.dto.GameItemDto;
 
 @WebServlet("*.do")
 public class Controler extends HttpServlet {
@@ -65,26 +54,6 @@ public class Controler extends HttpServlet {
 		case "storeContent":
 			GameContentCommand gameContentSelect = new GameContentSelectCommand();
 			gameContentSelect.execute(request, response);
-			GameContentDto dto =(GameContentDto) request.getAttribute("dto");
-			System.out.println("gameId : "+dto.getGameId());
-			System.out.println("itemSrc : "+dto.getItemSrc());
-			System.out.println("f1Src : "+dto.getF1Src());
-			System.out.println("f2Src : "+dto.getF2Src());
-			System.out.println("name : "+dto.getName());
-			System.out.println("genre : "+dto.getGenre());
-			System.out.println("date : "+dto.getDate());
-			System.out.println("ration : "+dto.getRation());
-			System.out.println("price : "+dto.getPrice());
-			System.out.println("content : "+dto.getContent());
-			System.out.println("link : "+dto.getLink());
-			System.out.println("mCpu : "+dto.getmCpu());
-			System.out.println("mGpu : "+dto.getmGpu());
-			System.out.println("mRam : "+dto.getmRam());
-			System.out.println("mDisk : "+dto.getmDisk());
-			System.out.println("sCpu : "+dto.getsCpu());
-			System.out.println("sGpu : "+dto.getsGpu());
-			System.out.println("sRam : "+dto.getsRam());
-			System.out.println("sDisk : "+dto.getsDisk());
 			viewPage="WEB-INF/view/storeContent.jsp";
 			break;
 		}
